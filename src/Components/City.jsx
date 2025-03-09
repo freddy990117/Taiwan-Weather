335b503a73b277865b10c6d7a264e6fc9b21efa7
Freddy990117 <Freddy990117@gmail.com>
2025-03-09 11:21:14 +0800

    成功拿到 API Data 與 command  StrictMode (防止重複執行結果) (main.jsx)

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 const APIKey = import.meta.env.VITE_API_KEY;
 const City = () => {
+  const [data, setData] = useState([]);
   const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${APIKey}&limit=2`;
 
-  return <div>City Data Over Here</div>;
+  async function fetchAPI() {
+    const result = await axios.get(API);
+    console.log(result.data.records.location);
+  }
+
+  fetchAPI();
+  return <div></div>;
 };
 
 export default City;
