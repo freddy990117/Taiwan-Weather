40041cfaf2fed60632c2bef790d54ae224cba2ff
Freddy990117 <Freddy990117@gmail.com>
2025-03-09 15:17:17 +0800

    新增 newData 存放迴圈後的資料

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -4,35 +4,30 @@ const APIKey = import.meta.env.VITE_API_KEY;
 const City = () => {
   const [data, setData] = useState([]);
   const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${APIKey}&limit=2`;
-  // 拿取 API 資料
 
+  // 拿取 API 資料
   async function fetchAPI() {
     let result = await axios.get(API);
     const location = result.data.records.location;
     const dataLength = location.length;
+    const newData = []; // 新增一個 Array 用來存放的資料
 
-    for (let i = 0; i <= dataLength - 1; i++) {
-      const city = result.data.records.location[i].locationName;
-      const Startweather =
-        result.data.records.location[i].weatherElement[0].time[0].startTime;
-      const EndWeather =
-        result.data.records.location[i].weatherElement[0].time[0].endTime;
+    for (let i = 0; i <= dataLength; i++) {
+      const city = location[i].locationName;
+      const Startweather = location[i].weatherElement[0].time[0].startTime;
+      const EndWeather = location[i].weatherElement[0].time[0].endTime;
       // 下雨機率
       const IsRain =
-        result.data.records.location[i].weatherElement[1].time[0].parameter
-          .parameterName + "%";
+        location[i].weatherElement[1].time[0].parameter.parameterName + "%";
       // 最低溫
       const MinTemperature =
-        result.data.records.location[i].weatherElement[2].time[0].parameter
-          .parameterName + "°C";
+        location[i].weatherElement[2].time[0].parameter.parameterName + "°C";
       // 舒適度
       const IsComfortable =
-        result.data.records.location[i].weatherElement[3].time[0].parameter
-          .parameterName;
+        location[i].weatherElement[3].time[0].parameter.parameterName;
       // 最高溫
       const MaxTemperature =
-        result.data.records.location[i].weatherElement[4].time[0].parameter
-          .parameterName + "°C";
+        location[i].weatherElement[4].time[0].parameter.parameterName + "°C";
 
       console.log("我是：", city);
       console.log("開始天氣時間是：", Startweather);
@@ -42,19 +37,20 @@ const City = () => {
       console.log("降雨機率：", IsRain);
       console.log("舒適度：", IsComfortable);
 
-      // setData(
-      //   city,
-      //   Startweather,
-      //   EndWeather,
-      //   MaxTemperature,
-      //   MinTemperature,
-      //   IsRain,
-      //   IsComfortable
-      // );
+      // 新增資料進入 Array
+      newData.push([
+        city,
+        Startweather,
+        EndWeather,
+        MaxTemperature,
+        MinTemperature,
+        IsRain,
+        IsComfortable,
+      ]);
     }
 
-    console.log(result.data.records.location);
-    // 用 map + concat 的方式新增資料嗎？
+    // 更新 data 狀態，避免無限渲染
+    setData(newData);
   }
   fetchAPI();
 
