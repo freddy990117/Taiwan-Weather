3347cb51d32f438ff48b7c0453b0b82f4a360729
Freddy990117 <Freddy990117@gmail.com>
2025-03-09 10:37:36 +0800

    將 img 與 section 的高度統一

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -3,16 +3,15 @@ import "../styles/style.css";
 const Homepage = () => {
   return (
     <div>
+      <section className="homepageImage">
+        <img src="../images/Taiwan.jpg" alt="Taiwan Mountain" />
+        {/* <figcaption>Alishan Ｍountain</figcaption> */}
+        <h1>全台天氣概況</h1>
+        <h2>點我選城市</h2>
+      </section>
       <section>{/* City Images Data */}</section>
     </div>
   );
 };
 
 export default Homepage;
-
-<section className="homepageImage">
-  <img src="../images/Taiwan.jpg" alt="Taiwan Mountain" />
-  {/* <figcaption>Alishan Ｍountain</figcaption> */}
-  <h1>全台天氣概況</h1>
-  <h2>點我選城市</h2>
-</section>;
