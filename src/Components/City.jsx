5935199ca374f05840d998e86be4eeb5018f9a7e
Freddy990117 <Freddy990117@gmail.com>
2025-03-09 14:06:05 +0800

    已將 API 資料截取出來，需設定迴圈與

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -7,11 +7,46 @@ const City = () => {
 
   async function fetchAPI() {
     const result = await axios.get(API);
-    console.log(result.data.records.location);
-  }
+    const city = result.data.records.location[0].locationName;
+    const Startweather =
+      result.data.records.location[0].weatherElement[0].time[0].startTime;
+    const EndWeather =
+      result.data.records.location[0].weatherElement[0].time[0].endTime;
+    // 下雨機率
+    const IsRain =
+      result.data.records.location[0].weatherElement[1].time[0].parameter
+        .parameterName + "%";
+    // 最低溫
+    const MinTemperature =
+      result.data.records.location[0].weatherElement[2].time[0].parameter
+        .parameterName + "°C";
+    // 舒適度
+    const IsComfortable =
+      result.data.records.location[0].weatherElement[3].time[0].parameter
+        .parameterName;
+    // 最高溫
+    const MaxTemperature =
+      result.data.records.location[0].weatherElement[4].time[0].parameter
+        .parameterName + "°C";
 
+    console.log(result.data.records.location);
+    console.log("我是：", city);
+    console.log("我的開始天氣時間是：", Startweather);
+    console.log("我是結束天氣時間是：", EndWeather);
+    console.log("最高溫：", MaxTemperature);
+    console.log("最低溫：", MinTemperature);
+    console.log("降雨機率：", IsRain);
+    console.log("舒適度：", IsComfortable);
+  }
   fetchAPI();
+
   return <div></div>;
 };
 
 export default City;
+
+// Wx 天氣現象
+// MaxT 最高溫度
+// MinT 最低溫度
+// CI 舒適度
+// PoP 降雨機率
