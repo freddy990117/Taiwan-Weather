8de56ae102ee329a36949533d7b8616fa9983ea8
Freddy990117 <Freddy990117@gmail.com>
2025-03-09 10:39:10 +0800

    新增 Axios Package

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import axios from "axios";
+const APIKey = import.meta.env.VITE_API_KEY;
 const City = () => {
-  return <div>City</div>;
+  const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${APIKey}&limit=2`;
+
+  return <div>City Data Over Here</div>;
 };
 
 export default City;
