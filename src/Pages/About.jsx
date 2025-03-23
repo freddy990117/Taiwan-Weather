2ea6b62cd7526caa0d721d09b851a5760aaf1a18
Freddy990117 <Freddy990117@gmail.com>
2025-03-23 14:22:52 +0800

    加入 About 的位置

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,11 +3,26 @@ import "../styles/about.css";
 const About = () => {
   return (
     <div>
-      <section className="About-Image">Image position</section>
-      <section className="Now-Weather">現在天氣</section>
-      <section className="Futer-Weather">天氣預測</section>
-      <section className="Futer-Rain">下雨機率預測</section>
-      <section className="Futer-Tem">一週天氣預測</section>
+      <section className="about about-image">
+        <h1>
+          <span className="city-name">City Name</span>
+        </h1>
+      </section>
+      <section className="about now-weather">
+        <h1>現在天氣</h1>
+        <div className="about-weather">
+          <div className="about-wind">天氣現象</div>
+          <div className="about-temp">
+            平均暈度
+            <span className="temp"></span>
+          </div>
+          <div className="about-rain">降雨機率</div>
+          <span className="rain"></span>
+        </div>
+      </section>
+      <section className="about future-weather">天氣預測</section>
+      <section className="about future-rain">下雨機率預測</section>
+      <section className="about future-temp">一週天氣預測</section>
     </div>
   );
 };
