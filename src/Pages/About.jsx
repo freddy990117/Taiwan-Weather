d67be87504786e1fdbf04705aaaca28d3b97d9b9
Freddy990117 <Freddy990117@gmail.com>
2025-03-23 11:25:34 +0800

    新增 About.jsx 資訊與 about.css

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import "../styles/about.css";
 const About = () => {
-  return <div>
-  <h1>About</h1>
-  <h1>About</h1>
-  <h1>About</h1>
-  <h1>About</h1>
-  <h1>About</h1>
-  <h1>About</h1>
-  <h1>About</h1>
-  </div>;
+  return (
+    <div>
+      <section className="About-Image">Image position</section>
+      <section className="Now-Weather">現在天氣</section>
+      <section className="Futer-Weather">天氣預測</section>
+      <section className="Futer-Rain">下雨機率預測</section>
+      <section className="Futer-Tem">一週天氣預測</section>
+    </div>
+  );
 };
 
 export default About;
