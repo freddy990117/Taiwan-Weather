779d117556c7f86ed8e2972a17649a423b3e4974
Freddy990117 <Freddy990117@gmail.com>
2025-03-25 06:37:07 +0800

    在 About.jsx 中需要使用 Homepag.jsx 的 API Data，思考我該怎麼處理 🤔

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import "../styles/about.css";
 const About = () => {
   return (
@@ -13,22 +13,23 @@ const About = () => {
         <h1>現在天氣</h1>
         <div className="about-weather">
           <div className="about-wind">
-            天氣現象
-            <span className="icon wind">ICON</span>
+            <h2>天氣現象</h2>
+            <span className="info wind">ICON</span>
           </div>
           <div className="about-temp">
-            平均溫度
-            <span className="icon temp">ICON</span>
+            <h2>平均溫度</h2>
+            <span className="info temp">ICON</span>
           </div>
           <div className="about-rain">
-            降雨機率
-            <span className="icon rain">ICON</span>
+            <h2>降雨機率</h2>
+            <span className="info rain">ICON</span>
           </div>
         </div>
       </section>
       {/* 天氣預測 */}
       <section className="about future-weather">
         <h1>天氣預測</h1>
+        {/* <button onClick={PrevBtn}>←</button> */}
         <div className="weather-nav">
           <div className="weather-detail">
             <div className="weather-date">Date</div>
@@ -67,6 +68,7 @@ const About = () => {
             <div className="weather-rain">Rain %</div>
           </div>
         </div>
+        {/* <button onClick={NextBtn}>→</button> */}
       </section>
       {/* 下雨機率 */}
       <section className="about future-rain">下雨機率預測</section>
