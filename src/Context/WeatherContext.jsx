a767a56555e668615d43a005709bc660a95c7950
Freddy990117 <Freddy990117@gmail.com>
2025-03-29 11:40:09 +0800

    更新 API 取得的天數 (1天->3天)

--- src/Context/WeatherContext.jsx
+++ src/Context/WeatherContext.jsx
@@ -15,21 +15,34 @@ export const WeatherProvider = ({ children }) => {
       const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${APIKey}`;
       let result = await axios.get(API);
       const location = result.data.records.location;
+      console.log(location);
 
-      const newData = location.map((cityData) => ({
-        city: cityData.locationName,
-        startTime: cityData.weatherElement[0].time[0].startTime,
-        endTime: cityData.weatherElement[0].time[0].endTime,
-        isRain:
-          cityData.weatherElement[1].time[0].parameter.parameterName + "%",
-        minTemp:
-          cityData.weatherElement[2].time[0].parameter.parameterName + "°C",
-        isComfortable:
-          cityData.weatherElement[3].time[0].parameter.parameterName,
-        maxTemp:
-          cityData.weatherElement[4].time[0].parameter.parameterName + "°C",
-      }));
+      // 建立 fetchAPI 的區域變數 (newData)
+      const newData = [];
 
+      location.forEach((cityData) => {
+        // 創建 forEach 的一個區域變數 (cityWeather)
+        const cityWeather = [];
+        // 這個 time 指的是該城市天氣要素（weatherElement）中的時間區間，每個天氣要素都有各自的 time 陣列
+        for (let i = 0; i < cityData.weatherElement[0].time.length; i++) {
+          // 將該城市的特定時間區間的天氣資訊加入 cityWeather 陣列 (Wx,Pop,MinT,Ci,MaxT)
+          cityWeather.push({
+            city: cityData.locationName,
+            startTime: cityData.weatherElement[0].time[i].startTime,
+            endTime: cityData.weatherElement[0].time[i].endTime,
+            isRain:
+              cityData.weatherElement[1].time[i].parameter.parameterName + "%",
+            minTemp:
+              cityData.weatherElement[2].time[i].parameter.parameterName + "°C",
+            isComfortable:
+              cityData.weatherElement[3].time[i].parameter.parameterName,
+            maxTemp:
+              cityData.weatherElement[4].time[i].parameter.parameterName + "°C",
+          });
+        }
+        newData.push(cityWeather); // 將整理好的區域變數（cityWeather）推入 fetchAPI 的區域變數 (newData) ,資料型態會是二維陣列 （[[0,1,2], [0,1,2], [0,1,2]]）
+      });
+      // 透過 hook (setData) 更新 data
       setData(newData);
     }
     fetchAPI();
