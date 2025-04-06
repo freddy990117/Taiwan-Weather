423914e5192b0668b382aedcd8a5df6f918dfc1a
Freddy990117 <Freddy990117@gmail.com>
2025-04-06 10:25:55 +0800

    1.Fetch 一週的 API 2.刪除舊的內容

--- src/Context/WeatherContext.jsx
+++ src/Context/WeatherContext.jsx
@@ -12,42 +12,38 @@ export const WeatherProvider = ({ children }) => {
   const [selectCity, setSelectCity] = useState(null); // 設定選擇的城市
   useEffect(() => {
     async function fetchAPI() {
-      const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${APIKey}`;
+      // 一週的 API
+      const API = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=${APIKey}`;
       let result = await axios.get(API);
-      const location = result.data.records.location;
+      const location = result.data.records.Locations[0].Location;
 
       // 建立 fetchAPI 的區域變數 (newData)
       const newData = [];
-
       location.forEach((cityData) => {
-        // 創建 forEach 的一個區域變數 (cityWeather)
         const cityWeather = [];
-        // time 指的是該城市天氣要素（weatherElement）中的時間區間，每個天氣要素都有各自的 time 陣列
-        for (let i = 0; i < cityData.weatherElement[0].time.length; i++) {
-          // 將該城市的特定時間區間的天氣資訊加入 cityWeather 陣列 (Wx,Pop,MinT,Ci,MaxT)
+        for (let i = 0; i < cityData.WeatherElement[14].Time.length; i++) {
           cityWeather.push({
-            city: cityData.locationName,
-            startTime: cityData.weatherElement[0].time[i].startTime,
-            endTime: cityData.weatherElement[0].time[i].endTime,
-            isRain:
-              cityData.weatherElement[1].time[i].parameter.parameterName + "%",
-            minTemp:
-              cityData.weatherElement[2].time[i].parameter.parameterName + "°C",
-            isComfortable:
-              cityData.weatherElement[3].time[i].parameter.parameterName,
-            maxTemp:
-              cityData.weatherElement[4].time[i].parameter.parameterName + "°C",
+            city: cityData.LocationName,
+            startTime: cityData.WeatherElement[14].Time[i].StartTime,
+            endTime: cityData.WeatherElement[14].Time[i].EndTime,
+            info: cityData.WeatherElement[14].Time[i].ElementValue[0]
+              .WeatherDescription,
           });
         }
-        newData.push(cityWeather); // 將整理好的區域變數（cityWeather）推入 fetchAPI 的區域變數 (newData) ,資料型態會是二維陣列 （[[0,1,2], [0,1,2], [0,1,2]]）
+        // 將整理好的區域變數（cityWeather）推入 fetchAPI 的區域變數 (newData) ,資料型態會是二維陣列 （[[0,1,2], [0,1,2], [0,1,2]]）
+        newData.push(cityWeather);
       });
-      // 透過 hook (setData) 更新 data
+      // 將整理好的資料加入到 Data 中
       setData(newData);
     }
     fetchAPI();
   }, []);
+
+  console.log("I am Data:", data);
   // data 是二維陣列，一個陣列中有該城市的三個資訊[ [1,2,3],[1,2,3] ]
+
   const firstElements = data.map((element) => element[0]); // 指取第一陣列的資訊
+  console.log(firstElements);
 
   return (
     // WeatherContext 提供 value (API's Data) for children，children 會在 Layout.jsx 帶入
