5f19ae617f898d4b3e9d51cb0e7f79332d04baeb
Freddy990117 <Freddy990117@gmail.com>
2025-06-01 08:33:54 +0800

    try try CI

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -9,7 +9,6 @@ const Homepage = () => {
   const { firstElements } = useContext(WeatherContext); // 取得 API 資料
   const [isOpen, setIsOpen] = useState(false);
   const [selectCity, setSelectCity] = useState(null);
-  console.log(data);
   // 指取第一陣列的資訊
   // const firstElements = data.map((element) => element[0]);
   return (
